3849928f62b0789a235cc3cd55549f05794f367a
Matteo <matteo@desktop-r7qhfk8.home>
2022-12-12 23:11:37 +0100

    [update]: try to add card with image of rank and username + rank and number of win

--- src/components/leaderboard/Leaderboard.tsx
+++ src/components/leaderboard/Leaderboard.tsx
@@ -1,6 +1,68 @@
 import React, { useEffect, useState } from 'react';
+import { ButtonBase, Grid, Typography, Paper } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import Api from '../../utils/api';
 import UserInfo from '../../type/LolTypes';
+import { Emblem, ValueRank } from '../../lol/Lol';
+/* const listEmblem = [
+  {
+    name: 'BRONZE',
+    img: bronze
+  }
+]; */
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%'
+});
+
+interface Test {
+  user: UserInfo;
+}
+
+function ComplexGrid({ user }: Test) {
+  return (
+    <Paper
+      sx={{
+        p: 2,
+        margin: 'auto',
+        width: 500,
+        flexGrow: 1,
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'dark' ? '#1A2027' : '#fff'
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item>
+          <ButtonBase sx={{ width: 128, height: 128 }}>
+            <Img alt="complex" src={Emblem[ValueRank.indexOf(user.tier)]} />
+          </ButtonBase>
+        </Grid>
+        <Grid item xs={12} sm container>
+          <Grid item xs container direction="column" spacing={2}>
+            <Grid item xs>
+              <Typography gutterBottom variant="subtitle1" component="div">
+                {user.summonerName}
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                {user.tier.charAt(0) +
+                  user.tier.substring(1).toLocaleLowerCase()}{' '}
+                {user.rank}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1" component="div">
+              {user.wins}W
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
 
 function Leaderboard() {
   const [dataLeaderboard, setDataLeaderboard] = useState([]);
@@ -16,15 +78,31 @@ function Leaderboard() {
     };
 
     fetchLeaderboard();
-  });
+  }, []);
 
   return (
-    <div>
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
       {dataLeaderboard.map((element: UserInfo, index) => {
         return (
-          // eslint-disable-next-line react/no-array-index-key
-          <div key={index}>
-            <div>{element.summonerName}</div>
+          <div
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <ComplexGrid user={element} />
           </div>
         );
       })}
---
+++ src/lol/Lol.ts
@@ -0,0 +1,36 @@
+import Bronze from './Emblem_Bronze.png';
+import Challenger from './Emblem_Challenger.png';
+import Diamond from './Emblem_Diamond.png';
+import Gold from './Emblem_Gold.png';
+import GrandMaster from './Emblem_Grandmaster.png';
+import Iron from './Emblem_Iron.png';
+import Master from './Emblem_Master.png';
+import Platinium from './Emblem_Platinum.png';
+import Silver from './Emblem_Silver.png';
+
+const ValueRank = [
+  'Iron',
+  'Bronze',
+  'Silver',
+  'Gold',
+  'Platinum',
+  'Diamond',
+  'Master',
+  'Grandmaster',
+  'Challenger'
+];
+
+const Emblem = [
+  Iron,
+  Bronze,
+  Silver,
+  Gold,
+  Platinium,
+  Diamond,
+  Master,
+  GrandMaster,
+  Challenger
+];
+// const valuePalier = ['IV', 'III', 'II', 'I'];
+
+export { Emblem, ValueRank };
